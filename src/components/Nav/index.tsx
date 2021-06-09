6bcf20d89d587c12373a060d5f35a43692f655ca
Marcelino <marceometry@gmail.com>
2021-06-09 16:54:50 -0300

    start user popup

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,27 +1,61 @@
-import Link from 'next/link'
-import { NavigateBefore, NavigateNext, Person, ArrowDropDown } from '@material-ui/icons'
+import Link from "next/link"
+import { useState } from "react"
+import { NavigateBefore, NavigateNext, Person, ArrowDropDown, ArrowDropUp } from "@material-ui/icons"
 
-import styles from './style.module.scss'
+import styles from "./style.module.scss"
 
 export default function Nav() {
-    return (
-        <nav className={styles.nav}>
-          <div className={styles.router}>
-            <NavigateBefore className={styles.disabled} />
-            <NavigateNext />
-          </div>
+  const [isUserPopUpOpen, setIsUserPopUpOpen] = useState(false)
 
-          <Link href="/">
-            <a className={styles.upgrade}>FAÇA UPGRADE</a>
-          </Link>
+  function openUserPopUp() {
+    setIsUserPopUpOpen(!isUserPopUpOpen)
+  }
 
-          <button className={styles.user}>
-            <Person />
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.router}>
+        <NavigateBefore className={styles.disabled} />
+        <NavigateNext />
+      </div>
 
-            <span>Marcelino Teixeira</span>
+      <Link href="/">
+        <a className={styles.upgrade}>FAÇA UPGRADE</a>
+      </Link>
 
-            <ArrowDropDown />
-          </button>
-        </nav>
-    )
+      <button className={styles.user} onClick={openUserPopUp}>
+        <Person />
+
+        <span>Marcelino Teixeira</span>
+
+        {isUserPopUpOpen ? (
+          <>
+          <ArrowDropUp />
+          
+          <ul className={styles.userPopUp}>
+            <li>
+              <Link href="/">
+                <a> Conta </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a> Perfil </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a> Faça upgrade para o Premium </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a> Sair </a>
+              </Link>
+            </li>
+          </ul>
+          </>
+        ) : <ArrowDropDown />}
+      </button>
+    </nav>
+  );
 }
