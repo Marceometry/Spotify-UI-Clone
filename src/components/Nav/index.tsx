54457d74485461f3fa9a06d0fafe17e76263a007
Marcelino <marceometry@gmail.com>
2021-06-10 17:00:27 -0300

    adjust dropdown

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,15 +1,30 @@
 import Link from "next/link"
-import { useState } from "react"
+import Image from "next/image"
+import { useState, useEffect, useRef } from "react"
 import { NavigateBefore, NavigateNext, Person, ArrowDropDown, ArrowDropUp } from "@material-ui/icons"
 
 import styles from "./style.module.scss"
 
 export default function Nav() {
   const [isUserPopUpOpen, setIsUserPopUpOpen] = useState(false)
+  const ref = useRef(null)
 
   function openUserPopUp() {
     setIsUserPopUpOpen(!isUserPopUpOpen)
   }
+
+  useEffect(() => {
+      function handleClickOutside(event) {
+          if (ref.current && !ref.current.contains(event.target)) {
+              setIsUserPopUpOpen(false)
+          }
+      }
+
+      document.addEventListener("mousedown", handleClickOutside)
+      return () => {
+          document.removeEventListener("mousedown", handleClickOutside)
+      }
+  }, [ref])
 
   return (
     <nav className={styles.nav}>
@@ -22,39 +37,58 @@ export default function Nav() {
         <a className={styles.upgrade}>FAÇA UPGRADE</a>
       </Link>
 
-      <button className={styles.user} onClick={openUserPopUp}>
+      <button
+        ref={ref}
+        className={styles.user}
+        onClick={openUserPopUp}
+        style={isUserPopUpOpen ? {background: "var(--gray-30)"} : {}}
+      >
         <Person />
 
-        <span>Marcelino Teixeira</span>
+        <span className={styles.userName}>Marcelino Teixeira</span>
 
-        {isUserPopUpOpen ? (
-          <>
-          <ArrowDropUp />
-          
-          <ul className={styles.userPopUp}>
-            <li>
-              <Link href="/">
-                <a> <span>Conta</span>  <img src="/external-link.svg" /> </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a> <span>Perfil</span>  </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a> <span>Faça upgrade para o Premium</span>  <img src="/external-link.svg" /> </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/">
-                <a> <span>Sair</span>  </a>
-              </Link>
-            </li>
-          </ul>
-          </>
-        ) : <ArrowDropDown />}
+        <span
+          className={`${styles.arrow} ${isUserPopUpOpen ? styles.arrowUp : styles.arrowDown}`}>
+        </span>
+        
+        <ul
+          className={styles.userPopUp}
+          style={isUserPopUpOpen ? {
+            transform: 'scaleY(1)',
+            padding: '5px'
+          } : {}}
+        >
+          <li>
+            <Link href="/">
+              <a>
+                <span>Conta</span>
+                <Image width={32} height={32} src="/external-link.svg" alt="" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>
+                <span>Perfil</span>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>
+                <span>Faça upgrade para o Premium</span>
+                <Image width={32} height={32} src="/external-link.svg" alt="" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>
+                <span>Sair</span>
+              </a>
+            </Link>
+          </li>
+        </ul>
       </button>
     </nav>
   );
