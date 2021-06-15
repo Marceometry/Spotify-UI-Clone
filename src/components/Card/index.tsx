ff31185799d490c1b63175aeca159ebbadad3d81
Marcelino Teixeira <marceometry@gmail.com>
2021-06-15 11:07:13 -0300

    fix cards row

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,4 @@
 import { PlayArrow } from '@material-ui/icons'
-import Image from 'next/image'
 import Link from 'next/link'
 import styles from './style.module.scss'
 
@@ -9,7 +8,7 @@ export default function Card() {
         <Link href="/">
             <a className={styles.card}>
                 <div className={styles.img}>
-                    <Image width={100} height={100} src="/music-note.png" alt="Imagem" />
+                    <img src="/music-note.png" alt="Imagem" />
 
                     <div className={styles.playIcon}>
                         <PlayArrow />
