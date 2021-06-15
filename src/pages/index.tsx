e2573a8efe8a4621109261226ea164c23c9ea716
Marcelino Teixeira <marceometry@gmail.com>
2021-06-15 18:06:17 -0300

    add page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import CardsRow from '../components/CardsRow'
 import { fakeData } from '../utils/fakeData'
 import styles from '../css/Home.module.scss'
@@ -5,6 +6,10 @@ import styles from '../css/Home.module.scss'
 export default function Home() {  
   return (
     <div className={styles.rowsContainer}>
+      <Head>
+        <title>Spotify - Web Player</title>
+      </Head>
+
       {fakeData.map((item, key) => (
         <CardsRow data={item} key={key} />
       ))}
