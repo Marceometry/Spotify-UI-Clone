be57425c51f2a8b27cc5834efb21de4d753a5b68
Marcelino Teixeira <marceometry@gmail.com>
2021-06-22 19:00:02 -0300

    add debounce and clear function to search input

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useRef, useState } from "react"
+import debounce from "lodash.debounce"
 import { Search, Close } from "@material-ui/icons"
 import styles from "./style.module.scss"
 
@@ -7,28 +8,34 @@ export default function SearchInput() {
     const [value, setValue] = useState('')
     const [dbValue, saveToDb] = useState('')
 
-    // const debouncedSave = useCallback(
-    //     debounce(nextValue => saveToDb(nextValue), 1000),
-    //     [], // will be created only once initially
-    // )
-    // // highlight-ends
+    const debouncedSave = useCallback(
+        debounce(nextValue => console.log(nextValue), 400),
+        [], // will be created only once initially
+    )
+    // highlight-ends
     
-    // const handleChange = event => {
-    //     const { value: nextValue } = event.target;
-    //     setValue(nextValue);
-    //     // Even though handleChange is created on each render and executed
-    //     // it references the same debouncedSave that was created initially
-    //     debouncedSave(nextValue);
-    // }
+    const handleChange = target => {
+        const { value: nextValue } = target
+        setValue(nextValue)
+        // Even though handleChange is created on each render and executed
+        // it references the same debouncedSave that was created initially
+        debouncedSave(nextValue)
+    }
+
+    function clearInput() {
+      inputRef.current.value = ''
+      inputRef.current.focus()
+      setValue('')
+    }
 
     return (
         <fieldset className={styles.searchInput}>
           <Search />
-          <input ref={inputRef} type="text" name="search"
+          <input onChange={() => handleChange(event.target)} ref={inputRef} type="text" name="search"
             placeholder="Artistas, músicas ou podcasts"
             maxLength={80} autoCorrect="off" spellCheck="false"
           />
-          {inputRef.current && <Close />}
+          {inputRef.current && value && <Close onClick={clearInput} />}
         </fieldset>
     )
 }
