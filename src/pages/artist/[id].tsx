0935321802919f06317c48028cce79a1d3d0fc38
Marcelino Teixeira <marceometry@gmail.com>
2021-07-05 10:49:19 -0400

    finish artist page

--- src/pages/artist/[id].tsx
+++ src/pages/artist/[id].tsx
@@ -1,34 +1,16 @@
 import Head from 'next/head'
 import { GetServerSideProps } from 'next'
-import { useEffect, useState } from 'react'
-import { getArtist } from '../api/spotifyAPI'
+import { getArtist, search } from '../api/spotifyAPI'
 
-import playlistDurationToString from '../../utils/playlistDurationToString'
-
-import { FavoriteBorderOutlined, MoreHoriz, Schedule } from '@material-ui/icons'
+import { Add, MoreHoriz } from '@material-ui/icons'
 import PlayArrowGreen from '../../components/PlayArrowGreen'
 import TrackRow from '../../components/TrackRow'
 
 import styles from '../../css/Playlist.module.scss'
+import tracksStyles from '../../css/Tracks.module.scss'
+import CardsRow from '../../components/CardsRow'
 
-export default function Playlist({ artist }) {
-    // const [playlistDuration, setPlaylistDuration] = useState(0)
-
-    // useEffect(() => {
-    //     if (!artist.tracks.items) {
-    //         return
-    //     } else {
-    //         let duration = 0
-    
-    //         artist.tracks.items.map(item => (
-    //             duration += item.track.duration_ms
-    //         ))
-
-    //         setPlaylistDuration(duration / 1000)
-    //     }
-    // }, [artist])
-
-    console.log(artist)
+export default function Playlist({ artist, tracks, albums }) {
 
     if (!artist.name) return <div className="loaderContainer"><i>Houve algo de errado ao carregar esta página</i></div>
 
@@ -38,25 +20,15 @@ export default function Playlist({ artist }) {
                 <title>Spotify - {artist.name}</title>
             </Head>
 
-            {/* <header style={{
-                background: `${artist.primary_color ? 
-                    `linear-gradient(${artist.primary_color}, ${artist.primary_color}45 100%)`
-                    : 'linear-gradient(#808080, #80808045 100%)'
-                }`,
-                boxShadow: `${artist.primary_color ?
-                    `0 8px 200px 80px ${artist.primary_color}45`
-                    : '0 8px 200px 80px #80808045'
-                }`
+            <header style={{
+                background: 'linear-gradient(#808080, #80808045 100%)',
+                boxShadow: '0 8px 200px 80px #80808045'
             }}>
                 <img src={artist.images[0].url} alt="Imagem" />
 
                 <div>
-                    <strong>PLAYLIST</strong>
-
                     <h1>{artist.name}</h1>
-                    <p>{artist.description}</p>
-
-                    <span><strong>{artist.owner.display_name}</strong> - {artist.tracks.total} músicas, {playlistDurationToString(playlistDuration)}.</span>
+                    <p>{artist.followers.total.toLocaleString()} seguidores</p>
                 </div>
             </header>
 
@@ -64,47 +36,43 @@ export default function Playlist({ artist }) {
                 <div className={styles.sectionHeader}>
                     <PlayArrowGreen />
                     
-                    <button><FavoriteBorderOutlined /></button>
+                    <button><Add /></button>
                     
                     <button><MoreHoriz /></button>
                 </div>
 
-                <div className={styles.playlist}>
-                    <div className={styles.playlistHeader}>
-                        <span>#</span>
-                        <span>TÍTULO</span>
-                        <span>ÁLBUM</span>
-                        <span>ADICIONADO EM</span>
-                        <span><Schedule /> </span>
-                    </div>
+                <div className={tracksStyles.tracks}>
+                    <h1>Populares</h1>
 
                     <ul>
-                        {artist.tracks.items.map((item, key) => (
+                        {tracks.slice(0, 5).map((item, key) => (
                             <TrackRow key={key}
                                 index={key + 1}
                                 track={{
-                                    id: item.track.id,
-                                    url: item.track.preview_url,
-                                    name: item.track.name,
-                                    artists: item.track.artists.map(artist => {
+                                    id: item.id,
+                                    url: item.preview_url,
+                                    name: item.name,
+                                    artists: item.artists.map(artist => {
                                         return {
                                             name: artist.name,
                                             id: artist.id
                                         }
-                                    }) ,
+                                    }),
                                     album: {
-                                        id: item.track.album.id,
-                                        name: item.track.album.name,
-                                        img: item.track.album.images[0].url
+                                        id: item.album.id,
+                                        name: item.album.name,
+                                        img: item.album.images[0].url
                                     },
                                     addedAt: item.added_at,
-                                    duration: item.track.duration_ms,
+                                    duration: item.duration_ms,
                                 }}
                             />
                         ))}
                     </ul>
                 </div>
-            </section> */}
+
+                <CardsRow data={albums} />
+            </section>
         </div>
     )
 }
@@ -113,7 +81,15 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     const { id } = params
     const artist = await getArtist(id)
 
+    const { tracksResult, albumsResult } = await search(artist.name)
+
+    const tracks = tracksResult.items.filter(item => item.artists[0].name === artist.name)
+    const albums = {
+        title: 'Álbuns',
+        items: albumsResult.items.filter(item => item.artists[0].name === artist.name)
+    }
+
     return {
-        props: { artist }
+        props: { artist, tracks, albums }
     }
 }
